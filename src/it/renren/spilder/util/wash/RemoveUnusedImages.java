ec117e13511a5fe039754275aea43efda2693e29
fenglibin@gmail.com <fenglibin@gmail.com@e56d1d59-6c2e-3479-e23e-0cb7336b9b4b>
2011-04-29 05:12:44 +0000

    改善连接方式

--- src/it/renren/spilder/util/wash/RemoveUnusedImages.java
+++ src/it/renren/spilder/util/wash/RemoveUnusedImages.java
@@ -1,9 +1,9 @@
 package it.renren.spilder.util.wash;
 
 import java.io.File;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import javax.sql.DataSource;
 
@@ -12,63 +12,64 @@ import it.renren.spilder.main.Constants;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+/**
+ * …æ≥˝»ﬂ”‡µƒÕº∆¨ ¿‡RemoveUnusedImages.javaµƒ µœ÷√Ë ˆ£∫TODO ¿‡ µœ÷√Ë ˆ
+ * 
+ * @author fenglibin 2011-4-29 œ¬ŒÁ01:00:59
+ */
 public class RemoveUnusedImages {
 
-	private static final ConfigurableApplicationContext ctx = new FileSystemXmlApplicationContext(
-			new String[] { Constants.SPRING_CONFIG_FILE });
+    private static final ConfigurableApplicationContext ctx           = new FileSystemXmlApplicationContext(
+                                                                                                            new String[] { Constants.SPRING_CONFIG_FILE });
+    private static Connection                           connection    = null;
+    private static Connection                           fanConnection = null;
 
-	/**
-	 * @param args
-	 * @throws SQLException
-	 */
-	public static void main(String[] args) throws SQLException {
-		String imageDir = "/home/fenglibin/www/www.renren.it/uploads/allimg";
-		checkImages(imageDir);
+    /**
+     * @param args
+     * @throws SQLException
+     */
+    public static void main(String[] args) throws SQLException {
+        String imageDir = "/home/fenglibin/www/www.renren.it/uploads/allimg";
+        connection = ((DataSource) ctx.getBean("dataSource")).getConnection();
+        fanConnection = ((DataSource) ctx.getBean("dataSourceFanti")).getConnection();
+        checkImages(imageDir);
+        connection.close();
+        fanConnection.close();
 
-	}
+    }
 
-	public static void checkImages(String imagesDir) throws SQLException {
-		File file = new File(imagesDir);
-		File[] files = file.listFiles();
-		for (File thisFile : files) {
-			if (thisFile.isDirectory()) {
-				checkImages(thisFile.getAbsolutePath());
-			} else {
-				String name = thisFile.getName();
-				String namesmall = name.toLowerCase();
-				// ÷ªºÏ≤‚Õº∆¨
-				if (!(namesmall.endsWith("jpg") || namesmall.endsWith("png")
-						|| namesmall.endsWith("bmp") || namesmall
-						.endsWith("gif"))) {
-					return;
-				}
-				Statement st = ((DataSource) ctx.getBean("dataSource"))
-						.getConnection().createStatement();
-
-				ResultSet rs = st
-						.executeQuery("select aid from renrenaddonarticle where body like '%"
-								+ name + "%'");
-				boolean delete = false;
-				if (!rs.next()) {
-					Statement stFan = ((DataSource) ctx
-							.getBean("dataSourceFanti")).getConnection()
-							.createStatement();
-					ResultSet rsFan = stFan
-							.executeQuery("select aid from renrenfanti_addonarticle where body like '%"
-									+ name + "%'");
-					if (!rsFan.next()) {
-						delete = true;
-					}
-					stFan.close();
-				}
-				if (delete) {
-					System.out.println("delete " + thisFile.getAbsolutePath());
-					thisFile.delete();
-				}
-				st.close();
-
-			}
-		}
-	}
+    private static void checkImages(String imagesDir) throws SQLException {
+        File file = new File(imagesDir);
+        File[] files = file.listFiles();
+        for (File thisFile : files) {
+            if (thisFile.isDirectory()) {
+                checkImages(thisFile.getAbsolutePath());
+            } else {
+                String name = thisFile.getName();
+                String namesmall = name.toLowerCase();
+                // ÷ªºÏ≤‚Õº∆¨
+                if (namesmall.endsWith("jpg") || namesmall.endsWith("png") || namesmall.endsWith("bmp")
+                    || namesmall.endsWith("gif")) {
+                    System.out.println("Starting deal image " + thisFile.getAbsolutePath());
+                    ResultSet rs = connection.createStatement().executeQuery(
+                                                                             "select aid from renrenaddonarticle where body like '%"
+                                                                                     + name + "%'");
+                    boolean delete = false;
+                    if (!rs.next()) {
+                        ResultSet rsFan = fanConnection.createStatement().executeQuery(
+                                                                                       "select aid from renrenfanti_addonarticle where body like '%"
+                                                                                               + name + "%'");
+                        if (!rsFan.next()) {
+                            delete = true;
+                        }
+                    }
+                    if (delete) {
+                        System.out.println("delete " + thisFile.getAbsolutePath());
+                        thisFile.delete();
+                    }
+                }
+            }
+        }
+    }
 
 }
